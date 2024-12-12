204dbef895e2467ec9dd1eac4560f7360d308580
Rahul-aithal <aithalrahul34@gmail.com>
2024-12-12 12:39:10 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -2,32 +2,30 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+const app = express();
 
-const app=express();
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGN,
-    credentials:true,
-    
-}));
-
-app.use(express.json({limit:"16kb"}));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: false, limit: "16kb" }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(cookieParser());
 
 //Routes
 
-import farmerRouter from './Routers/farmer.router.js'
-import buyerRouter from './Routers/buyer.router.js'
+import farmerRouter from "./Routers/farmer.router.js";
+import buyerRouter from "./Routers/buyer.router.js";
 
-app.get("/test",(req,res)=>{
+app.get("/test", (req, res) => {
     res.send("HELLO WOLRD");
 });
 //Routes declartion
-app.use('/api/farmer',farmerRouter);
-app.use('/api/buyer',buyerRouter);
+app.use("/api/farmer", farmerRouter);
+app.use("/api/buyer", buyerRouter);
 
-
-
-export {app}
+export { app };
